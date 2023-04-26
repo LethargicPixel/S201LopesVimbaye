18f3605f57627917b24ab459965176ccace822ce
Mathéo Lopes <matheo.lopes@etu.unilim.fr>
2023-04-26 11:11:38 +0200

    implementation de lenumeration face

--- src/model/Face.java
+++ src/model/Face.java
@@ -1,5 +1,21 @@
 package model;
 
-public interface Face {
-//TODO
+public enum Face {
+COULEUR1("Pommes"),
+COULEUR2("Poires"),
+COULEUR3("Abricots"),
+COULEUR4("Peches"),
+COULEUR5("corbeau"),
+COULEUR6("bonus");
+
+private String values;
+
+private Face(String values) {
+	this.values=values;
+}
+
+public String toText() {
+	return this.values;
+}
+
 }
